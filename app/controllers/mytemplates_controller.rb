6fabab25002f09c708750b2dc6ede17013c5e186
Hyun Soo Kim <cholubin@gmail.com>
2010-10-04 17:46:42 +0900

    update

--- app/controllers/mytemplates_controller.rb
+++ app/controllers/mytemplates_controller.rb
@@ -37,9 +37,9 @@ class MytemplatesController < ApplicationController
     elsif cate == "all" and folder != "all"
       @mytemplates = Mytemplate.all(:folder => Tempfolder.get(folder).name, :user_id => current_user.id, :order => [:created_at.desc]).search(params[:search], params[:page])                                   
     elsif cate != "all" and folder == "all"
-      @mytemplates = Mytemplate.all(:category => Category.get(cate.to_i).name, :user_id => current_user.id, :order => [:created_at.desc]).search(params[:search], params[:page])                                   
+      @mytemplates = Mytemplate.all(:category => cate, :user_id => current_user.id, :order => [:created_at.desc]).search(params[:search], params[:page])                                   
     elsif cate != "all" and folder != "all"
-      @mytemplates = Mytemplate.all(:folder => Tempfolder.get(folder).name, :category => Category.get(cate.to_i).name, :user_id => current_user.id, :order => [:created_at.desc]).search(params[:search], params[:page])                           
+      @mytemplates = Mytemplate.all(:folder => Tempfolder.get(folder).name, :category => cate, :user_id => current_user.id, :order => [:created_at.desc]).search(params[:search], params[:page])                           
     end
     
     @tempfolders = Tempfolder.all(:user_id => current_user.id)
